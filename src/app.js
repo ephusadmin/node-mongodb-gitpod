0e903e1fa0647f982fbe09bd80d86ae4c288eea7
ephusadmin <edjey99@gmail.com>
2020-04-06 06:05:14 +0000

    add and list finished

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const Task = require('./model/Task');
+
+// settings
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    res.render('index', {tasks });
+})
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//Create Task
+app.post('/create', async(req, res)=>{
+    const newTask = new Task({
+        task: req.body.task,
+        description: req.body.description
+    })
+    await newTask.save();
+    res.redirect('/');
+})
+
+//Static Files
+app.use(express.static(__dirname + '/public'));
+
+module.exports = app;
+
